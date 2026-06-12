da1803cb38ec259f8dcb21a678612e19903d6b41
Agusmaru <97960364+Agusmaru@users.noreply.github.com>
2026-06-12 01:04:40 -0300

    Add files via upload
    Se agrega DetalleCompraNegocio(a chequear luego la vista final deseada)
    Se agrega ProveedorNegocio

---
+++ Negocio/DetalleCompraNegocio.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+using negocio;
+
+namespace Negocio
+{
+    public class DetalleCompraNegocio
+    {
+        public List<DetalleCompra> Listar()
+        {
+            List<DetalleCompra> lista = new List<DetalleCompra>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearProcedimiento("storedListarCompraDetalles");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    DetalleCompra aux = new DetalleCompra();
+
+                    aux.id = (int)datos.Lector["id_detalle"];
+
+                    aux.compra = new Compra();
+                    aux.compra.id = (int)datos.Lector["id_compra"];
+
+                    aux.producto = new Producto();
+                    aux.producto.id = (int)datos.Lector["id_producto"];
+
+                    aux.cantidad = float.Parse(datos.Lector["cantidad"].ToString());
+                    aux.precioUnitario = float.Parse(datos.Lector["precio_unitario"].ToString());
+                    aux.subtotal = float.Parse(datos.Lector["subtotal"].ToString());
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
---
+++ Negocio/ProveedorNegocio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+using negocio;
+
+namespace Negocio
+{
+    public class ProveedorNegocio
+    {
+        public List<Proveedor> Listar()
+        {
+            List<Proveedor> lista = new List<Proveedor>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearProcedimiento("storedListarProveedores");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Proveedor aux = new Proveedor();
+
+                    aux.id = (int)datos.Lector["id_proveedor"];
+                    aux.cuil = (string)datos.Lector["cuil"];
+                    aux.nombre = (string)datos.Lector["nombre"];
+                    aux.email = (string)datos.Lector["email"];
+                    aux.telefono = (string)datos.Lector["telefono"];
+                    aux.direccion = (string)datos.Lector["direccion"];
+                    aux.activo = (bool)datos.Lector["activo"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
